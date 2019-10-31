fc9bda662bc8bb64a048a5c029d17f2225dd68d5
GUOKUN LAI <laiguokun@163.com>
2019-10-31 14:24:27 -0400

    add type id

--- pretrain/seq2seq_edit_tf.py
+++ pretrain/seq2seq_edit_tf.py
@@ -8,12 +8,34 @@ import tensorflow as tf
 tf.enable_eager_execution()
 
 
+def get_type_id(tgt_len, tgt_idx, type_val):
+  tgt_idx_left_shift = tgt_idx[:-1]
+  type_val_right_shift = type_val[1:]
+  new_type_id = tf.scatter_nd(
+    shape=[tgt_len],
+    indices=tgt_idx[:, None],
+    updates=type_val
+  )
+  new_type_id = tf.reverse(new_type_id, axis=[0])
+  new_type_id = tf.math.cumsum(new_type_id)
+  new_type_id_shift = tf.scatter_nd(
+    shape=[tgt_len],
+    indices=tgt_idx_left_shift[:, None],
+    updates=type_val_right_shift
+  )
+  new_type_id_shift = tf.reverse(new_type_id_shift, axis=[0])
+  new_type_id_shift = tf.math.cumsum(new_type_id_shift)
+  new_type_id = new_type_id - new_type_id_shift
+  new_type_id = tf.reverse(new_type_id, axis=[0])
+  return new_type_id
+
 def main(argv):
   if len(argv) > 1:
     raise app.UsageError("Too many command-line arguments.")
 
   seq_len = 16
   inputs = tf.range(1, seq_len + 1, 1, dtype=tf.int32)
+  type_id = tf.range(1, seq_len + 1, 1, dtype=tf.int32)
 
   del_ratio = 0.1
   add_ratio = 0.1
@@ -66,8 +88,11 @@ def main(argv):
   # remove deleted token
   tgt_idx = tf.boolean_mask(shift_idx, tf.logical_and(non_del_mask, valid_tgt))
   tgt_val = tf.boolean_mask(rep_input, tf.logical_and(non_del_mask, valid_tgt))
+  type_val = tf.boolean_mask(type_id, tf.logical_and(non_del_mask, valid_tgt))
 
   max_len = tf.math.reduce_max(tgt_idx) + 1
+  
+  enc_type_id = get_type_id(tgt_len, tgt_idx, type_val)
 
   enc_seq = tf.scatter_nd(
       shape=[tgt_len],
@@ -81,6 +106,7 @@ def main(argv):
 
   print("encoder input")
   print(enc_seq.numpy().tolist())
+  print(enc_type_id.numpy().tolist())
 
   #### decoder
   shift_val = tf.cumsum(add_cnt)
@@ -91,12 +117,15 @@ def main(argv):
 
   tgt_idx = tf.boolean_mask(shift_idx, valid_tgt)
   tgt_val = tf.boolean_mask(inputs, valid_tgt)
+  type_val = tf.boolean_mask(type_id, valid_tgt)
 
   max_len = tf.math.reduce_max(tgt_idx) + 1
 
   pad_id = 100
   eos_id = 101
   add_id = 102
+
+  dec_type_id = get_type_id(tgt_len, tgt_idx, type_val)
 
   dec_seq = tf.concat(
       [tf.zeros(shape=[max_len], dtype=tf.int32) + add_id,
@@ -129,6 +158,7 @@ def main(argv):
   print("inputs", dec_inp.numpy().tolist())
   print("target", dec_seq.numpy().tolist())
   print("labels", edit_label.numpy().tolist())
+  print("type_id", dec_type_id.numpy().tolist())
 
 
 if __name__ == "__main__":
